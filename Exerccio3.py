0ff7590412e9208ffed2f304a082f746c67c8fa3
Leonardo Cherubino <96987724+LeonardoCherubino@users.noreply.github.com>
2022-07-24 20:42:30 -0300

    Exerccio3.py

---
+++ Exerccio3.py
@@ -0,0 +1,5 @@
+#  Faça um Programa que peça dois números e imprima a soma.
+Numero1 = int(input('Entre com o primeiro número: '))
+Numero2 = int(input('Entre com o segundo número '))
+Resultado = Numero1 + Numero2
+print('A soma dos números digitados é: {}'.format(Resultado))
